ea1254862e049c1f0ccb3da5a4db8253e8b7b649
AndyAtmadja03S <atmandy345@gmail.com>
2026-03-11 01:36:52 +1100

    Moved card component to components

--- app/pages/ProjectPage.tsx
+++ app/pages/ProjectPage.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Link from "next/link"
+import ProjectCard from "@/app/components/ProjectCard"
 import projects from "@/app/data/projects.json"
 
 interface Project {
@@ -28,40 +28,5 @@ export default function Project() {
         </div>
       </div>
     </div>
-  )
-}
-
-function ProjectCard({ project }: { project: Project }) {
-  return (
-    <Link href={project.link} target="_blank" className="group block">
-      <div className="cursor-pointer">
-        <div
-          className="relative w-full h-112.5 rounded-2xl overflow-hidden 
-          shadow-md transition-all duration-300 
-          group-hover:-translate-y-2 group-hover:shadow-xl
-          bg-white flex items-center justify-center"
-        >
-
-          <img
-            src={project.bg}
-            className="absolute inset-0 w-full h-full object-cover opacity-75"
-          />
-
-          <img
-            src={project.img}
-            alt={project.title}
-            className="relative w-32 h-32 object-contain"
-          />
-
-        </div>
-
-        <h3 className="text-2xl font-medium mt-6 mb-2">
-          {project.title}
-        </h3>
-        <p className="text-gray-600">
-          {project.description}
-        </p>
-      </div>
-    </Link>
   )
 }
