7e8a9269aefdabd513ef9173bae09fb29b531406
AndyAtmadja03S <atmandy345@gmail.com>
2026-03-25 22:36:10 +1100

    Fixed project page

--- app/pages/ProjectPage.tsx
+++ app/pages/ProjectPage.tsx
@@ -24,7 +24,7 @@ export default function Project() {
           Featured Projects
         </p>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-            {(projects as Project[]).map((project) => (
+            {(projects as Project[]).slice(0, 3).map((project) => (
               <ProjectCard key={project.id} project={project} />
             ))}
           </div>
